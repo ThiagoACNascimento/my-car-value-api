ebe34850b471ad7970120b7e6da3830e5eac96a6
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-16 21:21:29 -0300

    feat: update `auth.service.spec` and test `signup` function

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,21 +3,44 @@ import { AuthServices } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-it('can create and instance of auth service', async () => {
-  const fakeUserService: Partial<UsersService> = {
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+describe('AuthService', () => {
+  let service = AuthServices;
+  beforeEach(async () => {
+    const fakeUserService: Partial<UsersService> = {
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthServices,
-      { provide: UsersService, useValue: fakeUserService },
-    ],
-  }).compile();
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthServices,
+        { provide: UsersService, useValue: fakeUserService },
+      ],
+    }).compile();
 
-  const service = module.get(AuthServices);
+    service = module.get(AuthServices);
+  });
 
-  expect(service).toBeDefined();
+  it('can create and instance of auth service', () => {
+    expect(service).toBeDefined();
+  });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('test@test.com', 'teste123');
+
+    console.log(user);
+    expect(user).toEqual({
+      id: 1,
+      email: 'test@test.com',
+      password: user.password,
+    });
+
+    expect(user.password).not.toEqual('teste123');
+
+    const [salt, hash] = user.password.split('.');
+
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
 });
