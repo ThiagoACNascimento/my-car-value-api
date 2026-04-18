30f0a5eb1578e1b1e29cf240a068393e64290f7c
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-18 12:19:12 -0300

    chore: move `cookieSession` from `main` to `app.module`

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import cookieSession from 'cookie-session';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(
-    cookieSession({
-      keys: ['asdfasf'],
-    }),
-  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
