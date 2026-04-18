928a0376c54512e369f0b266412bb9f99266e0c9
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-18 11:10:59 -0300

    feat: add new tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -3,6 +3,7 @@ import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { AuthServices } from './auth.service';
 import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
@@ -42,5 +43,22 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    fakeUserService.findOne = () => null;
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUserAll returns a list of users with the given email', async () => {
+    const users = await controller.findAllUser('adasf@fadsdf.dsfsd');
+
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('adasf@fadsdf.dsfsd');
   });
 });
