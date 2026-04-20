14c362fdf0fd2e8b290b126d915a658312de7f82
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-20 14:28:58 -0300

    feat: add new dto to couple `userId` into `Report` entity

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -4,6 +4,8 @@ import { CreateReportDto } from './dtos/create-reports.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { currentUser } from 'src/users/decorators/current-user.decorator';
 import type { User } from 'src/users/user.entity';
+import { ReportDto } from './dtos/report.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,6 +13,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
+  @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @currentUser() user: User) {
     return this.reportService.create(user, body);
   }
