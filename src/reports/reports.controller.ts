6f7fb23b1e36381e0631ff278d1f8414bbbe57d1
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-20 14:11:14 -0300

    feat: add the relationship in `reports.controller` and `reports.service`

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-reports.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
+import { currentUser } from 'src/users/decorators/current-user.decorator';
+import type { User } from 'src/users/user.entity';
 
 @Controller('reports')
 export class ReportsController {
@@ -9,7 +11,7 @@ export class ReportsController {
 
   @Post()
   @UseGuards(AuthGuard)
-  createReport(@Body() body: CreateReportDto) {
-    return this.reportService.create(body);
+  createReport(@Body() body: CreateReportDto, @currentUser() user: User) {
+    return this.reportService.create(user, body);
   }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,14 +3,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './reports.entity';
 import { CreateReportDto } from './dtos/create-reports.dto';
+import type { User } from 'src/users/user.entity';
 
 @Injectable()
 export class ReportsService {
   constructor(@InjectRepository(Report) private repo: Repository<Report>) {}
 
-  create(reportDto: CreateReportDto) {
+  create(user: User, reportDto: CreateReportDto) {
     const report = this.repo.create(reportDto);
-
+    report.user = user;
     return this.repo.save(report);
   }
 }
