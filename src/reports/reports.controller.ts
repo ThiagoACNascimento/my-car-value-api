31dd0b3b0328141613ae0ca9bb8a6f43ad45b96b
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-20 14:50:55 -0300

    feat: add new route and column to approve the report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UseGuards,
+  Patch,
+  Param,
+} from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-reports.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
@@ -6,6 +13,7 @@ import { currentUser } from 'src/users/decorators/current-user.decorator';
 import type { User } from 'src/users/user.entity';
 import { ReportDto } from './dtos/report.dto';
 import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { ApproveReportDto } from './dtos/approve-report.dto';
 
 @Controller('reports')
 export class ReportsController {
@@ -16,5 +24,10 @@ export class ReportsController {
   @Serialize(ReportDto)
   createReport(@Body() body: CreateReportDto, @currentUser() user: User) {
     return this.reportService.create(user, body);
+  }
+
+  @Patch('/:id')
+  approveReport(@Param('id') id: string, @Body() body: ApproveReportDto) {
+    return this.reportService.changeApproval(id, body.approved);
   }
 }
