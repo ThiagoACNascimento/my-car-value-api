4569f24733fd1a236599ae76f1ad310bffb1eb1b
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-21 13:16:46 -0300

    feat: add `CurrentUserMiddleware` and `AdminGuard`

---
+++ src/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+import { UsersService } from '../users.service';
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+  constructor(private userService: UsersService) {}
+
+  async use(request: Request, response: Response, next: NextFunction) {
+    const { userId } = request.session || {};
+
+    if (userId) {
+      const user = await this.userService.findOne(userId);
+      // @ts-ignore
+      request.currentUser = user;
+    }
+
+    next();
+  }
+}
