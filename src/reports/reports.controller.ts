23fc955742cc2b17a570a31e064d591d55f12049
Thiago Costa <thiagoacnascimento@gmail.com>
2026-04-21 13:51:29 -0300

    feat: add new controller to get a car price estimate

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -5,9 +5,12 @@ import {
   UseGuards,
   Patch,
   Param,
+  Get,
+  Query,
 } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { CreateReportDto } from './dtos/create-reports.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { currentUser } from 'src/users/decorators/current-user.decorator';
 import type { User } from 'src/users/user.entity';
@@ -26,6 +29,9 @@ export class ReportsController {
   createReport(@Body() body: CreateReportDto, @currentUser() user: User) {
     return this.reportService.create(user, body);
   }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {}
 
   @Patch('/:id')
   @UseGuards(AdminGuard)
